514ace10a34b59f4b5ce95fa55c870fb4404c16b
Mr Stark <102538726+Mr-Stark-RDJ@users.noreply.github.com>
2024-08-30 12:13:39 +0530

    Yahoo

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import *
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+userData = {}
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+  return render_template('home.html',message=request.cookies.get('Token').title())
+
+
+
+@app.route('/register',methods = ['POST', 'GET'])  
+def register():
+  if request.method == 'POST':
+      username = request.form['username'].lower()  
+      password = bcrypt.generate_password_hash(request.form['password']) 
+      error=''
+      if username in userData.keys():
+        error='username already exit\n try new username'
+        return render_template('signup.html',error=error)
+      else:
+        userData[username] = password 
+        return redirect('/login')
+  elif request.method == 'GET':
+    return render_template('signup.html')
+         
+
+@app.route('/login',methods = ['POST','GET'])  
+def login():  
+  if request.method == "POST":  
+    username = request.form['username'].lower()  
+    password = request.form['password']
+    if request.cookies.get('Token') == username:    
+        return redirect('/home')
+    else:
+      print(f"{username} {password} {userData}")
+      if username in userData.keys():
+        print('working')
+        if bcrypt.check_password_hash(userData[username], password) :
+          print('working fine')
+          resp = make_response(redirect('/home'))
+          resp.set_cookie('Token',username)
+          print('working smoothly')
+          return resp
+        else:
+          return render_template('login.html',error="Wrong password")
+      else:
+        return render_template('login.html', error="Username doesn't exist")
+  elif request.method == "GET":
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
---
+++ static/script.js
@@ -0,0 +1,24 @@
+function checkPasswordLength(password){
+    if (password.length < 8 || password.length>16) {
+        document.getElementById("errorSpan").innerHTML = "Password length must be greater than 8 and less than 16";
+        document.getElementById("signupbtn").disabled = true;
+    }
+    else {
+        document.getElementById("errorSpan").innerHTML = "";
+    } 
+}
+
+var check = function() {
+if (document.getElementById('password').value == document.getElementById('checkPassword').value) {
+        document.getElementById("signupbtn").disabled = false;
+        document.getElementById('signupbtn').style.backgroundColor = '#3498db';
+        document.getElementById('alertPassword').style.color = '#8CC63E';
+        document.getElementById('alertPassword').innerHTML = '<span><i class="fas fa-check-circle"></i>Match !</span>';
+} 
+else {
+        document.getElementById("signupbtn").disabled = true;
+        document.getElementById('signupbtn').style.backgroundColor = '#3498db96';
+        document.getElementById('alertPassword').style.color = '#EE2B39';
+        document.getElementById('alertPassword').innerHTML = '<span><i class="fas fa-exclamation-triangle"></i>not matching</span>';
+}
+}
